e11d28ca9b4546dc65d46f11f0bd734f23aa2888
Eric Chan <ericchan@Erics-MacBook-Pro.local>
2017-08-29 19:14:56 -0400

    working copy

--- src/main/java/com/photostore/demo/config/DynamoDBConfig.java
+++ src/main/java/com/photostore/demo/config/DynamoDBConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
@@ -25,11 +27,33 @@ public class DynamoDBConfig {
  
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
+    	/* Approach 1
+    	AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(amazonAWSCredentials());
+    	
+    	ddbClient.setEndpoint(amazonDynamoDBEndpoint);
+    	ddbClient.setRegion(Region.getRegion(Regions.US_EAST_2));  
+    	
+    	return ddbClient;
+    	*/
+    	
+    	/* Apporach 2
+    	AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+    	        amazonDynamoDBEndpoint, Regions.US_EAST_2.getName());
+
+    	    AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(amazonAWSCredentials());
+
+    	    AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+    	        endpointConfiguration).withCredentials(credentialsProvider).build();
+
+    	    return amazonDynamoDB;*/
+    	
+    	    
         AmazonDynamoDB amazonDynamoDB 
           = new AmazonDynamoDBClient(amazonAWSCredentials());
          
         if (amazonDynamoDBEndpoint != null && !amazonDynamoDBEndpoint.isEmpty()) {
             amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+            amazonDynamoDB.setRegion(Region.getRegion(Regions.US_EAST_2));
         }
         return amazonDynamoDB;
     }
--- src/main/java/com/photostore/demo/controller/PhotoController.java
+++ src/main/java/com/photostore/demo/controller/PhotoController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.photostore.demo.domain.PhotoStore;
 import com.photostore.demo.repositories.PhotoStorageRepository;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping("test")
-public class RestController {
+public class PhotoController {
 	
 	@Autowired PhotoStorageRepository repo;
 
